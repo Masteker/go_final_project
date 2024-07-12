aad989cae7a3b04ebd645210497b0cd444b0c657
Denis Tupalov <Denis.tupalov62@gmail.com>
2024-07-12 15:33:28 +0000

    начал с 0.

--- database/db/shema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "scheduler" (
-	"id" INTEGER PRIMARY KEY AUTOINCREMENT,
-	"date" DATE NOT NULL DEFAULT "",
-	"title" VARCHAR(128) NOT NULL DEFAULT "",
-	"comment" TEXT NOT NULL DEFAULT "",
-	"repeat" TEXT NOT NULL DEFAULT ""
-);
-
-CREATE INDEX date_index ON scheduler (date);
